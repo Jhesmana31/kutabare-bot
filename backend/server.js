e8d5bef4a0211923a9853140eecec446d91975d2
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-18 21:32:04 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+const connectDB = require('./config/db');
+const app = express();
+const productRoutes = require('./routes/product');
+const orderRoutes = require('./routes/order');
 require('dotenv').config();
 
-const orderRoutes = require('./routes/orders');
-
-const app = express();
-app.use(cors());
+connectDB();
 app.use(express.json());
 
+app.use('/api/products', productRoutes);
 app.use('/api/orders', orderRoutes);
 
-const PORT = process.env.PORT || 5000;
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}).catch(err => console.error(err));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
