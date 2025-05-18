c30208f508a5b4bc745d8b4f5ba056a503424a17
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-18 20:20:14 +0800

    Update config.js

--- telegram-bot/config.js
+++ telegram-bot/config.js
@@ -1,5 +1,4 @@
 module.exports = {
   BOT_TOKEN: '7368568730:AAHbnlzq6a3aSxrFstJ12caHiUmn8aW7txw',
   ADMIN_ID: '7699555744',
-  API_BASE_URL: 'https://your-backend-url.onrender.com', // Update this later
 };
