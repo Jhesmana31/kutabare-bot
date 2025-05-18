18705840776e0863f2b373703cea51163e897c39
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-18 20:19:45 +0800

    Update bot.js

--- telegram-bot/bot.js
+++ telegram-bot/bot.js
@@ -1,6 +1,6 @@
 const { Bot } = require('grammy');
 const config = require('./config');
-const { handleStart, handleOrder } = require('./services/handlers');
+const { handleStart, handleOrder } = require('./orderHandler');
 
 const bot = new Bot(config.BOT_TOKEN);
 
@@ -8,4 +8,4 @@ bot.command('start', handleStart);
 bot.on('message:text', handleOrder);
 
 bot.start();
-console.log('Bot is running...');
+console.log('Telegram bot is running...');
