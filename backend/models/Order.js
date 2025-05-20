f48db66608f5d96592f21ab0db55f6ffb3188fc8
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-21 07:25:30 +0800

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,21 +1,9 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  telegramId: { type: String, required: true },
-  name: String,
-  contact: String,
-  deliveryOption: String,
-  address: String,
-  products: [
-    {
-      name: String,
-      price: Number,
-      quantity: Number,
-    }
-  ],
-  totalAmount: Number,
-  paymentStatus: { type: String, default: 'pending' },
-  createdAt: { type: Date, default: Date.now },
+  chatId: { type: String, required: true },
+  message: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Order', orderSchema);
