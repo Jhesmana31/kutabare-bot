8ececaeca209d587ceddc90d0ea54010da40e2de
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-20 10:51:13 +0800

    Update and rename products.json to products.js

---
+++ telegram-bot/products/products.js
@@ -0,0 +1,107 @@
+module.exports = [
+  {
+    name: 'Cock Ring - Pack of 3',
+    category: 'Cock Rings & Toys',
+    price: 80
+  },
+  {
+    name: 'Cock Ring Vibrator',
+    category: 'Cock Rings & Toys',
+    price: 60
+  },
+  {
+    name: 'Spikey Jelly (Red)',
+    category: 'Cock Rings & Toys',
+    price: 160
+  },
+  {
+    name: 'Spikey Jelly (Black)',
+    category: 'Cock Rings & Toys',
+    price: 160
+  },
+  {
+    name: '"Th Bolitas" Jelly',
+    category: 'Cock Rings & Toys',
+    price: 160
+  },
+  {
+    name: 'Monogatari Lube Tube',
+    category: 'Lubes & Condoms',
+    price: 120
+  },
+  {
+    name: 'Monogatari Lube Pinhole',
+    category: 'Lubes & Condoms',
+    price: 120
+  },
+  {
+    name: 'Monogatari Flavored Lube',
+    category: 'Lubes & Condoms',
+    price: 200,
+    variants: ['Peach', 'Strawberry', 'Cherry']
+  },
+  {
+    name: 'Maxman per Tab',
+    category: 'Performance Enhancers',
+    price: 40
+  },
+  {
+    name: 'Maxman per Pad',
+    category: 'Performance Enhancers',
+    price: 400
+  },
+  {
+    name: 'Ultra thin 001 Condoms',
+    category: 'Lubes & Condoms',
+    price: 90,
+    variants: ['Black', 'Long Battle', 'Blue', 'Naked Pleasure', 'Granule Passion']
+  },
+  {
+    name: 'Portable Wired Vibrator Egg',
+    category: 'Cock Rings & Toys',
+    price: 130
+  },
+  {
+    name: 'Delay Collar',
+    category: 'Spicy Accessories',
+    price: 200
+  },
+  {
+    name: 'Delay Ejaculation Buttplug',
+    category: 'Spicy Accessories',
+    price: 200
+  },
+  {
+    name: 'Eucalyptus Menthol Food Grade',
+    category: 'Essentials',
+    variants: ['15-20 (₱1000)', '25-30 (₱1500)', '35-40 (₱2000)'],
+    price: 1000
+  },
+  {
+    name: '7 Inches African Version Dildo',
+    category: 'Cock Rings & Toys',
+    price: 270
+  },
+  {
+    name: 'Masturbator Cup',
+    category: 'Cock Rings & Toys',
+    price: 120,
+    variants: ['Yellow (Mouth)', 'Gray (Arse)', 'Black (Vagina)']
+  },
+  {
+    name: 'Mouth Fresheners',
+    category: 'Essentials',
+    price: 90,
+    variants: ['Peach', 'Mint']
+  },
+  {
+    name: 'Insulin Syringe',
+    category: 'Essentials',
+    price: 20
+  },
+  {
+    name: 'Sterile Water for Injection',
+    category: 'Essentials',
+    price: 15
+  }
+];
