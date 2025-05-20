4e1882be10f22de75b964f24b931f3b103eaa9b9
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-21 00:20:37 +0800

    Delete kutabare-dashboard/lib directory

--- kutabare-dashboard/lib/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-const API_BASE = 'https://kutabare-backend.onrender.com/api'
-
-export const getOrders = async () => {
-  const res = await axios.get(`${API_BASE}/orders`)
-  return res.data
-}
