f534a75d5e92a5463bbb900dbbb49efe079eb7a4
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-20 08:25:03 +0800

    Update config.js

--- telegram-bot/config.js
+++ telegram-bot/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BOT_TOKEN: '7368568730:AAHbnlzq6a3aSxrFstJ12caHiUmn8aW7txw',
-  ADMIN_ID: '7699555744',
+  BOT_TOKEN: "7368568730:AAHbnlzq6a3aSxrFstJ12caHiUmn8aW7txw",
+  ADMIN_ID: "7699555744"
 };
