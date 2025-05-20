d340f1917ba3cc2457ea0a5859d007a3208c59ad
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-20 08:30:16 +0800

    Update and rename server.js to index.js

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,12 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+  useUnifiedTopology: true,
+})
+.then(() => {
   console.log('MongoDB connected');
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-}).catch((err) => console.error('DB connection error:', err));
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+})
+.catch(err => console.error(err));
