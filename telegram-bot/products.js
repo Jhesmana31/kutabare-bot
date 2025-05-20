a9629ddefbc1123e159804c627619ace97ba759b
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-21 07:53:49 +0800

    Create products.js

---
+++ telegram-bot/products.js
@@ -0,0 +1,33 @@
+// products.js
+
+module.exports = {
+  "Cock Rings & Toys": [
+    { name: "Cock Ring - Pack of 3", price: 80 },
+    { name: "Cock Ring Vibrator", price: 60 },
+    { name: "Spikey Jelly", price: 160, variants: ["Red", "Black"] },
+    { name: "\"Th Bolitas\" Jelly", price: 160 },
+    { name: "Portable Wired Vibrator Egg", price: 130 },
+    { name: "7 Inches African Version Dildo", price: 270 },
+    { name: "Masturbator Cup", price: 120, variants: ["Yellow (Mouth)", "Gray (Arse)", "Black (Vagina)"] },
+  ],
+  "Lubes & Condoms": [
+    { name: "Monogatari Lube Tube", price: 120 },
+    { name: "Monogatari Lube Pinhole", price: 120 },
+    { name: "Monogatari Flavored Lube", price: 200, variants: ["Peach", "Strawberry", "Cherry"] },
+    { name: "Ultra thin 001 Condom", price: 90, variants: ["Black", "Long Battle", "Blue", "Naked Pleasure", "Granule Passion"] },
+  ],
+  "Performance Enhancers": [
+    { name: "Maxman per Tab", price: 40 },
+    { name: "Maxman per Pad", price: 400 },
+  ],
+  "Spicy Accessories": [
+    { name: "Delay Collar", price: 200 },
+    { name: "Delay Ejaculation Buttplug", price: 200 },
+  ],
+  "Essentials": [
+    { name: "Eucalyptus Menthol Food Grade", price: 1000, variants: ["15-20 (1k)", "25-30 (1.5k)", "35-40 (2k)"] },
+    { name: "Mouth Fresheners", price: 90, variants: ["Peach", "Mint"] },
+    { name: "Insulin Syringe", price: 20 },
+    { name: "Sterile Water for Injection", price: 15 },
+  ]
+};
