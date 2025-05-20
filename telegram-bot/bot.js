037c0f24f5c1cb36a6d2aebb655a0e021f8329be
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-20 08:23:54 +0800

    Update bot.js

--- telegram-bot/bot.js
+++ telegram-bot/bot.js
@@ -1,11 +1,27 @@
-const { Bot } = require('grammy');
+const TelegramBot = require('node-telegram-bot-api');
+const products = require('./products');
+const orderHandler = require('./orderHandler');
 const config = require('./config');
-const { handleStart, handleOrder } = require('./orderHandler');
 
-const bot = new Bot(config.BOT_TOKEN);
+const bot = new TelegramBot(config.BOT_TOKEN, { polling: true });
 
-bot.command('start', handleStart);
-bot.on('message:text', handleOrder);
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  const categories = Object.keys(products);
+  const buttons = categories.map(cat => [{ text: cat }]);
+  bot.sendMessage(chatId, 'Choose a category:', {
+    reply_markup: {
+      keyboard: buttons,
+      resize_keyboard: true
+    }
+  });
+});
 
-bot.start();
-console.log('Telegram bot is running...');
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  if (products[msg.text]) {
+    orderHandler.handleCategory(bot, chatId, msg.text);
+  } else {
+    orderHandler.handleOrderFlow(bot, msg);
+  }
+});
