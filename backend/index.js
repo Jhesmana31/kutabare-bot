c1aff3a91eb66b53424654122ad5ec9d7e6ee266
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-22 01:02:12 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,10 +10,12 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
-
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
 // Add this route
 app.get('/', (req, res) => {
   res.send('Server is live!');
