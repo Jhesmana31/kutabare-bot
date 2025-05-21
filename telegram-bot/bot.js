d358cb8929b342932961f9240c104175d24db2a6
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-21 21:53:53 +0800

    Update bot.js

--- telegram-bot/bot.js
+++ telegram-bot/bot.js
@@ -221,6 +221,7 @@ bot.on('message', (msg) => {
 });
 
 // LISTEN
-app.listen(3000, () => {
-  console.log('Bot server running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Bot server running on port ${PORT}`);
 });
