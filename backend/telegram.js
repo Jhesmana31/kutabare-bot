6918adf6c3b3badb8bd0b0944f8f6ce5fb3ea5c0
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-22 02:44:03 +0800

    Update telegram.js

--- backend/telegram.js
+++ backend/telegram.js
@@ -1,11 +1,13 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const sendTelegramMessage = (chatId, text) => {
-  axios.post(`https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`, {
+const TELEGRAM_TOKEN = process.env.BOT_TOKEN;
+
+function sendTelegramMessage(chatId, text) {
+  return axios.post(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage`, {
     chat_id: chatId,
     text,
   });
-};
+}
 
 module.exports = { sendTelegramMessage };
