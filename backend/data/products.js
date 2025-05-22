4a6e118019644b7842b3b2fdc2535b05c63757d6
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-22 09:15:04 +0800

    Create products.js

---
+++ backend/data/products.js
@@ -0,0 +1,69 @@
+module.exports = {
+  categories: [
+    {
+      name: 'Cock Rings & Toys',
+      products: [
+        { name: 'Cock Ring - Pack of 3', price: 80 },
+        { name: 'Cock Ring Vibrator', price: 60 },
+        { name: 'Spikey Jelly (Red)', price: 160 },
+        { name: 'Spikey Jelly (Black)', price: 160 },
+        { name: '"Th Bolitas" Jelly', price: 160 },
+        { name: 'Portable Wired Vibrator Egg', price: 130 },
+        { name: '7 Inches African Version Dildo', price: 270 },
+        {
+          name: 'Masturbator Cup',
+          price: 120,
+          variants: ['Yellow (Mouth)', 'Gray (Arse)', 'Black (Vagina)']
+        }
+      ]
+    },
+    {
+      name: 'Lubes & Condoms',
+      products: [
+        { name: 'Monogatari Lube Tube', price: 120 },
+        { name: 'Monogatari Lube Pinhole', price: 120 },
+        {
+          name: 'Monogatari Flavored Lube',
+          price: 200,
+          variants: ['Peach', 'Strawberry', 'Cherry']
+        },
+        {
+          name: 'Ultra thin 001 Condoms',
+          price: 90,
+          variants: ['Black', 'Long Battle', 'Blue', 'Naked Pleasure', 'Granule Passion']
+        }
+      ]
+    },
+    {
+      name: 'Performance Enhancers',
+      products: [
+        { name: 'Maxman per Tab', price: 40 },
+        { name: 'Maxman per Pad', price: 400 }
+      ]
+    },
+    {
+      name: 'Spicy Accessories',
+      products: [
+        { name: 'Delay Collar', price: 200 },
+        { name: 'Delay Ejaculation Buttplug', price: 200 }
+      ]
+    },
+    {
+      name: 'Essentials',
+      products: [
+        {
+          name: 'Eucalyptus Menthol Food Grade',
+          price: 1000,
+          variants: ['15-20 (1k)', '25-30 (1.5k)', '35-40 (2k)']
+        },
+        {
+          name: 'Mouth Fresheners',
+          price: 90,
+          variants: ['Peach', 'Mint']
+        },
+        { name: 'Insulin Syringe', price: 20 },
+        { name: 'Sterile Water for Injection', price: 15 }
+      ]
+    }
+  ]
+};
