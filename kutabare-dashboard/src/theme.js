cb6d30e1a827580090a8e0ab60120e04f2827047
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-24 04:58:25 +0800

    Create theme.js

---
+++ kutabare-dashboard/src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#7b1fa2',  // violet-ish
+    },
+    secondary: {
+      main: '#f50057', // pink-ish
+    },
+  },
+});
+
+export default theme;
