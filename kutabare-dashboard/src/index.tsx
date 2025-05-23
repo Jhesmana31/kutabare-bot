acfc578642e1c9628026e8e3642c291612ca9550
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-24 05:02:24 +0800

    Update and rename index.js to index.tsx

--- kutabare-dashboard/src/index.tsx
+++ kutabare-dashboard/src/index.tsx
@@ -5,7 +5,7 @@ import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from './theme';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
