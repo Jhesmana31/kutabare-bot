94a5a1cbcc1b2e65b9007dacf152ccdea52d4e56
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-25 23:41:14 +0800

    Update orders.js

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const path = require('path');
-const Order = require('../models/order');
+const Order = require('../models/Order');
 
 const router = express.Router();
 
