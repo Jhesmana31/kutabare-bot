6c53faf0a47c19e1fb1fe6f551d8dd0b4a1cec65
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-26 00:37:56 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,10 @@ function isValidPhoneNumber(text) {
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB connected');
+}).catch(err => {
+  console.error('MongoDB connection error:', err);
 });
 
 // Categories and sample products map
