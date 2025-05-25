1f79d4fe8af20000f441db1908e51af9c28646c9
Jhesmana31 <j.esmana1993@gmail.com>
2025-05-25 23:44:57 +0800

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -3,24 +3,18 @@ const mongoose = require('mongoose');
 const ItemSchema = new mongoose.Schema({
   name: String,
   variant: { type: String, default: 'noVariant' },
-  price: Number,
   quantity: Number,
 });
 
 const OrderSchema = new mongoose.Schema({
   telegramId: { type: Number, required: true },
-  phone: String,
   items: [ItemSchema],
-  total: Number,
+  contact: String,
   deliveryOption: { type: String, default: 'Pickup' },
-  qrFile: String,
-  paymentProof: String,
-  paymentStatus: {
-    type: String,
-    enum: ['Pending', 'Awaiting Proof', 'Payment Confirmed', 'Rejected'],
-    default: 'Pending',
-  },
+  total: Number,
   createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.models.Order || mongoose.model('Order', OrderSchema);
+const Order = mongoose.models.Order || mongoose.model('Order', OrderSchema);
+
+module.exports = Order;
